b2a0b89442f54e3e171ec0195fada462914ba8c9
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-02-14 16:40:15 -0600

    Fixed up the code and comments in the mr_ash_demo.R script.

--- scripts/mr_ash_demo.R
+++ scripts/mr_ash_demo.R
@@ -1,4 +1,5 @@
-# TO DO: Explain here what this script does, and how to use it.
+# An illustration of the mr_ash algorithm applied to a small,
+# simulated data set.
 source("../code/misc.R")
 source("../code/mr_ash.R")
 
@@ -10,6 +11,10 @@ p  <- 1000
 sd <- c(0,    1,    2)
 w  <- c(0.98, 0.01, 0.01)
 
+# Variances for the mixture-of-normals prior on the regression
+# coefficients.
+s0 <- c(0.1,1,10)^2
+
 # SIMULATE DATA
 # -------------
 set.seed(1)
@@ -20,10 +25,17 @@ y    <- drop(X %*% beta + rnorm(n))
 
 # FIT MR-ASH MODEL
 # ----------------
-b0  <- rep(0,p)
+# These are the initial estimates of residual variance (s), mixture
+# weights (w0) and posterior mean estimates of the regression
+# coefficients (b).
+b   <- rep(0,p)
 s   <- 1
-s0  <- c(0.1,1,10)^2
 w0  <- c(0.5,0.25,0.25)
-fit <- mr_ash(X,y,s,s0,w0,b0,100)
+
+# Fit the model by running 100 EM updates.
+fit <- mr_ash(X,y,s,s0,w0,b,100)
+
+# Compare the posterior mean estimates against the values used to
+# simulate the data.
 plot(beta,fit$b,pch = 20,col = "black")
 abline(a = 0,b = 1,col = "skyblue",lty = "dotted")
