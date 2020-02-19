7c0bdc628ffd1b7671c1270913acd838e721adc5
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-02-19 16:16:40 -0600

    Implemented first trial of the mixsqp update for mr-ash.

--- scripts/mr_ash_demo.R
+++ scripts/mr_ash_demo.R
@@ -43,3 +43,33 @@ abline(a = 0,b = 1,col = "skyblue",lty = "dotted")
 # Plot the improvement in the solution over time.
 plot(max(fit$elbo) - fit$elbo + 1e-8,type = "l",col = "dodgerblue",
      lwd = 2,log = "y",xlab = "iteration",ylab = "distance to best ELBO")
+
+# TESTING
+# -------
+library(mixsqp)
+numiter <- 20
+k    <- 3
+se   <- s
+X    <- scale(X,scale = FALSE)
+elbo <- rep(0,numiter)
+for (iter in 1:numiter) {
+
+  # Update residual variance and posterior on coefficients.
+  fit <- mr_ash(X,y,se,s0,w0,b,100,tol = 1e-6,update.w0 = FALSE)
+  se  <- fit$se
+  b   <- fit$b
+  elbo[iter] <- max(fit$elbo)
+  
+  # Update mixture weights using mixsqp.
+  L <- matrix(0,p,k)
+  r <- drop(y - X %*% b)
+  for (i in 1:p) {
+    x <- X[,i]
+    r <- r + x*b[i]
+    for (j in 1:k)
+      L[i,j] <- bayes_lr_ridge(x,r,se,s0[j])$logbf
+    r <- r - x*b[i]
+  }
+  out <- mixsqp(L,log = TRUE,control = list(verbose = FALSE))
+  w0  <- out$x
+}
