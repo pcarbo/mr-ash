f24e69c8f321feb2e3f9ddc4ba0ccf5eb273b0b0
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-02-20 08:26:08 -0600

    Fixed up plots in test script.

--- scripts/test_mr_ash_with_mixsqp.R
+++ scripts/test_mr_ash_with_mixsqp.R
@@ -49,35 +49,37 @@ se  <- 1
 w0  <- rep(1/k,k)
 b   <- rep(0,p)
 
-# Running 20 EM updates to "pre-fit" the model.
-cat("Pre-fitting model by running 20 EM updates.\n")
-fit0 <- mr_ash(X,y,se,s0,w0,b,maxiter = 20,verbose = FALSE)
-
-# Fit the model by running 100 EM updates.
+# Fit the model by running 500 EM updates.
 cat("Running EM updates.\n")
-fit1 <- mr_ash(X,y,fit0$se,s0,fit0$w0,fit0$b,maxiter = 500,verbose = FALSE)
+fit1 <- mr_ash(X,y,se,s0,w0,b,maxiter = 500,verbose = FALSE)
 
 # Fit the model a second time using the "accelerated" mix-SQP updates
 # for the mixture weights.
 cat("Running 10 mix-SQP updates.\n")
-fit2 <- mr_ash_with_mixsqp(X,y,fit0$se,s0,fit0$w0,fit0$b,numiter = 10,
-                           tol.inner = 1e-8)
+fit2 <- mr_ash_with_mixsqp(X,y,se,s0,w0,b,numiter = 10,tol.inner = 1e-8)
 
 # REVIEW FITS
 # -----------
 # Compare the posterior mean estimates against the values used to
 # simulate the data.
 cat("Summarizing results.\n")
-p1 <- quickplot(beta,fit1$b) +
-  geom_abline(intercept = 0,slope = 1,col = "skyblue",lty = "dotted") +
-  theme_cowplot()
+p1 <- ggplot(data.frame(true = beta,mixsqp = fit2$b),
+             aes(x = true,y = mixsqp)) +
+  geom_point(color = "darkblue") +
+  geom_abline(intercept = 0,slope = 1,col = "magenta",lty = "dotted") +
+  labs(title = "coefs") +
+  theme_cowplot(10)
 
 # Compare the posterior mean estimates from the two fits.
-p2 <- quickplot(fit1$b,fit2$b) +
-  geom_abline(intercept = 0,slope = 1,col = "skyblue",lty = "dotted") +
-  theme_cowplot()
+p2 <- ggplot(data.frame(em = fit1$b,mixsqp = fit2$b),
+             aes(x = em,y = mixsqp)) +
+  geom_point(color = "darkblue") +
+  geom_abline(intercept = 0,slope = 1,col = "magenta",lty = "dotted") +
+  labs(x = "em",y = "mixsqp",title = "coefs") +
+  theme_cowplot(10)
 
-# Plot the improvement in the solution over time.
+# Plot the improvement in the solution over time. The EM updates are
+# shown in blue, and the mix-SQP updates are shown in orange.
 elbo.best <- max(c(fit1$elbo,fit2$elbo))
 pdat      <- rbind(data.frame(update = "em",
                               iter   = 1:length(fit1$elbo),
@@ -87,8 +89,12 @@ pdat      <- rbind(data.frame(update = "em",
                               elbo   = fit2$elbo))
 pdat$elbo <- elbo.best - pdat$elbo + 0.01
 p3 <- ggplot(pdat,aes(x = iter,y = elbo,color = update)) +
-  geom_line() +
-  geom_point() +
+  geom_line(show.legend = FALSE) +
+  geom_point(show.legend = FALSE) +
   scale_y_log10() +
-  scale_color_manual(values = c("darkblue","darkorange")) +
-  theme_cowplot()
+  scale_color_manual(values = c("royalblue","darkorange")) +
+  labs(x = "iteration",y = "distance to \"best\" ELBO") +
+  theme_cowplot(10)
+
+# Show all the plots.
+print(plot_grid(p1,p2,p3,ncol = 3,rel_widths = c(2,2,2.5)))
