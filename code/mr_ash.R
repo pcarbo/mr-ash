81986e027aab06ee46b5f8d74e460e1f191fd5e4
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-02-21 21:25:49 -0600

    Re-organized mr_ash.R code slightly.

--- code/mr_ash.R
+++ code/mr_ash.R
@@ -1,10 +1,81 @@
-# Fit the mr-ash model, in which the "naive" M-step updates for the
-# mixture weights are replaced by a mix-SQP update.
+# Perform EM updates for the mr-ash model.
 #
 # This implementation is meant to be "instructive"---that is, I've
 # tried to make the code as simple as possible, with an emphasis on
 # clarity. Very little effort has been devoted to making the
 # implementation efficient, or the code concise.
+mr_ash <- function (X, y, se, s0, w0, b, maxiter = 100, tol = 1e-8,
+                    update.se = TRUE, update.w0 = TRUE, verbose = TRUE) {
+
+  # Center X and y.
+  X <- scale(X,scale = FALSE)
+  y <- y - mean(y)
+
+  # These two variables are used to keep track of the algorithm's
+  # progress: "elbo" stores the value of the objective (the
+  # variational lower bound, or "ELBO") at each iteration; "maxd"
+  # stores the largest difference in the posterior mean coefficients
+  # between two successive iterations.
+  elbo <- rep(as.numeric(NA),maxiter)
+  maxd <- rep(as.numeric(NA),maxiter)
+
+  # Iterate the EM updates.
+  if (verbose)
+    cat("iter                elbo max|b-b'|\n")
+  for (i in 1:maxiter) {
+
+    # Save the current estimates of the posterior means.
+    b0 <- b
+    
+    # E STEP
+    # ------
+    # Update the posterior means of the regression coefficients via
+    # co-ordinate ascent.
+    out <- mr_ash_update(X,y,b,se,s0,w0)
+    b   <- out$b
+    
+    # Record the algorithm's progress.
+    elbo[i] <- out$elbo
+    maxd[i] <- max(abs(b - b0))
+
+    # M STEP
+    # ------
+    # Update the residual variance, if requested.
+    if (update.se)
+      se <- out$erss/n
+    
+    # Update the mixture weights, if requested.
+    if (update.w0)
+      w0 <- out$w0.em
+
+    # Report progress, if requested.
+    if (verbose)
+      cat(sprintf("%4d %0.12e %0.3e\n",i,elbo[i],maxd[i]))
+
+    # Stop if the largest change in the posterior means of the
+    # regression coefficients is small.
+    if (maxd[i] <= tol)
+      break
+  } 
+
+  # Return the updated posterior means of the regression coefficicents
+  # ("b"), the prior variances of the mixture components ("s0"), the
+  # updated mixture weights ("w0"), the updated residual variance
+  # ("se"), the value of the objective after the (approximate) E-step
+  # at each iteration ("elbo"), and the maximum change in the
+  # regression coefficients at each iteration ("maxd").
+  return(list(b    = b,
+              s0   = s0,
+              w0   = w0,
+              se   = se,
+              elbo = elbo[1:i],
+              maxd = maxd[1:i]))
+}
+
+# Fit the mr-ash model, in which the "naive" M-step updates for the
+# mixture weights are replaced by a mix-SQP update.
+#
+# This is a preliminary implementation and may not work in all cases.
 mr_ash_with_mixsqp <- function (X, y, se, s0, w0, b, numiter = 10,
                                 update.se = TRUE, maxiter.inner = 100,
                                 tol.inner = 1e-8, verbose = TRUE) {
@@ -80,80 +151,6 @@ mr_ash_with_mixsqp <- function (X, y, se, s0, w0, b, numiter = 10,
               elbo  = elbo,
               maxd  = maxd,
               niter = niter))
-}
-
-# Perform EM updates for the mr-ash model.
-#
-# This implementation is meant to be "instructive"---that is, I've
-# tried to make the code as simple as possible, with an emphasis on
-# clarity. Very little effort has been devoted to making the
-# implementation efficient, or the code concise.
-mr_ash <- function (X, y, se, s0, w0, b, maxiter = 100, tol = 1e-8,
-                    update.se = TRUE, update.w0 = TRUE, verbose = TRUE) {
-
-  # Center X and y.
-  X <- scale(X,scale = FALSE)
-  y <- y - mean(y)
-
-  # These two variables are used to keep track of the algorithm's
-  # progress: "elbo" stores the value of the objective (the
-  # variational lower bound, or "ELBO") at each iteration; "maxd"
-  # stores the largest difference in the posterior mean coefficients
-  # between two successive iterations.
-  elbo <- rep(as.numeric(NA),maxiter)
-  maxd <- rep(as.numeric(NA),maxiter)
-
-  # Iterate the EM updates.
-  if (verbose)
-    cat("iter                elbo max|b-b'|\n")
-  for (i in 1:maxiter) {
-
-    # Save the current estimates of the posterior means.
-    b0 <- b
-    
-    # E STEP
-    # ------
-    # Update the posterior means of the regression coefficients via
-    # co-ordinate ascent.
-    out <- mr_ash_update(X,y,b,se,s0,w0)
-    b   <- out$b
-    
-    # Record the algorithm's progress.
-    elbo[i] <- out$elbo
-    maxd[i] <- max(abs(b - b0))
-
-    # M STEP
-    # ------
-    # Update the residual variance, if requested.
-    if (update.se)
-      se <- out$erss/n
-    
-    # Update the mixture weights, if requested.
-    if (update.w0)
-      w0 <- out$w0.em
-
-    # Report progress, if requested.
-    if (verbose)
-      cat(sprintf("%4d %0.12e %0.3e\n",i,elbo[i],maxd[i]))
-
-    # Stop if the largest change in the posterior means of the
-    # regression coefficients is small.
-    if (maxd[i] <= tol)
-      break
-  } 
-
-  # Return the updated posterior means of the regression coefficicents
-  # ("b"), the prior variances of the mixture components ("s0"), the
-  # updated mixture weights ("w0"), the updated residual variance
-  # ("se"), the value of the objective after the (approximate) E-step
-  # at each iteration ("elbo"), and the maximum change in the
-  # regression coefficients at each iteration ("maxd").
-  return(list(b    = b,
-              s0   = s0,
-              w0   = w0,
-              se   = se,
-              elbo = elbo[1:i],
-              maxd = maxd[1:i]))
 }
 
 # Perform a single pass of the co-ordinate ascent updates for the
