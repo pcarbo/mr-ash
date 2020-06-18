98e963335df23a9cd306146357517ca0645f5be9
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-06-18 07:19:03 -0500

    Added new script, mr_ash_vs_lasso.R, and accompanying R code functions_for_mr_ash_vs_lasso.R.

---
+++ analysis/mr_ash_vs_lasso.R
@@ -0,0 +1,13 @@
+library(glmnet)
+library(mr.ash.alpha)
+set.seed(1)
+source("../code/functions_for_mr_ash_vs_lasso.R")
+
+# SCRIPT SETTINGS
+# ---------------
+ns <- 10 # Number of simulations.
+
+# Repeat for each simulation.
+for (i in 1:ns) {
+
+}
