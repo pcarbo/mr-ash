aface8552b84ec52a794868b33ddb7f5d9f47cd7
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-09-17 17:11:53 -0500

    Generalized ELBO calculations to p > 1 variables.

--- scripts/verify_elbo.R
+++ scripts/verify_elbo.R
@@ -3,14 +3,15 @@
 # Script settings.
 n  <- 75
 s  <- 3
-b  <- c(-1,0)
+b  <- c(-1,1,0,0)
 s0 <- c(0.001,0.01,0.1,1)
 w0 <- c(0.0001,0.9,0.05,0.05)
 w0 <- w0/sum(w0)
 
 # Simulate data.
 set.seed(1)
-X <- matrix(rnorm(2*n),n,2)
+p <- length(b)
+X <- matrix(rnorm(n*p),n,p)
 X <- scale(X,center = TRUE,scale = FALSE)
 y <- drop(X %*% b) + sqrt(s)*rnorm(n)
 y <- y - mean(y)
@@ -36,12 +37,13 @@ normalizelogweights <- function (logw) {
 }
 compute_elbo <- function (bhat, X, y, sigma, s0, w) {
   n     <- length(y)
+  p     <- length(bhat)
   K     <- length(s0)
   d     <- diag(crossprod(X))
-  alpha <- matrix(0,2,K)
-  mu    <- matrix(0,2,K)
-  s     <- matrix(0,2,K)
-  for (i in 1:2) {
+  alpha <- matrix(0,p,K)
+  mu    <- matrix(0,p,K)
+  s     <- matrix(0,p,K)
+  for (i in 1:p) {
     shat   <- sigma/sqrt(d[i])
     s1     <- 1/sqrt(1/shat^2 + 1/s0^2)
     mu1    <- (s1/shat)^2*bhat[i]
@@ -59,7 +61,7 @@ compute_elbo <- function (bhat, X, y, sigma, s0, w) {
   for (i in 1:K)
     elbo <- (elbo + sum(alpha[,i]*log(w[i] + 1e-15)) 
                   - sum(alpha[,i]*log(alpha[,i] + 1e-15)))
-  for (i in 2:K)
+  for (i in 1:K)
     elbo <- (elbo+(sum(alpha[,i]) + sum(alpha[,i]*log(s[,i]/(s0[i]^2))))/2
              - sum(alpha[,i]*(s[,i] + mu[,i]^2))/(s0[i]^2)/2)
   print(elbo,digits = 8)
@@ -67,14 +69,15 @@ compute_elbo <- function (bhat, X, y, sigma, s0, w) {
 }
 compute_elbo2 <- function (bhat, X, y, sigma, s0, w) {
   n     <- length(y)
+  p     <- length(bhat)
   K     <- length(s0)
   d     <- diag(crossprod(X))
-  alpha <- matrix(0,2,K)
-  mu    <- matrix(0,2,K)
-  s     <- matrix(0,2,K)
-  shat  <- rep(0,2)
-  llik  <- rep(0,2)
-  for (i in 1:2) {
+  alpha <- matrix(0,p,K)
+  mu    <- matrix(0,p,K)
+  s     <- matrix(0,p,K)
+  shat  <- rep(0,p)
+  llik  <- rep(0,p)
+  for (i in 1:p) {
     shat[i] <- sigma/sqrt(d[i])
     s1     <- 1/sqrt(1/shat[i]^2 + 1/s0^2)
     mu1    <- (s1/shat[i])^2*bhat[i]
@@ -89,13 +92,13 @@ compute_elbo2 <- function (bhat, X, y, sigma, s0, w) {
   }
   b <- rowSums(alpha * mu)
   print(b)
-  elbo <- n/2*log(2*pi*sigma^2) - norm2(y - X %*% b)^2/(2*sigma^2) +
+  elbo <- n*(p-1)/2*log(2*pi*sigma^2) - norm2(y - X %*% b)^2/(2*sigma^2) +
           sum(llik) + sum(((bhat - b)^2 + sum(y^2)/d - bhat^2)/(2*shat^2))
-  print(elbo,digits = 8)
   return(elbo)
 }
-theta0 <- rnorm(2)
+theta0 <- rnorm(p)
 out <- optim(theta0,function (par) -compute_elbo2(par,X,y,s,s0,w0),
              method = "Nelder-Mead",control = list(reltol = 1e-10))
 print(best)
 print(max(fit$logZ),digits = 10)
+print(-out$value,digits = 10)
