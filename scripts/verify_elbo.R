f446d9c22f77541c598c8c018bd6710d495502cc
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-09-17 15:21:53 -0500

    Improved compute_elbo in verify_elbo.R.

--- scripts/verify_elbo.R
+++ scripts/verify_elbo.R
@@ -34,38 +34,39 @@ normalizelogweights <- function (logw) {
   w <- exp(logw - c)
   return(w/sum(w))
 }
-compute_elbo <- function (bhat, X, y, sigma, sa, w) {
+compute_elbo <- function (bhat, X, y, sigma, s0, w) {
   n     <- length(y)
-  K     <- length(sa)
+  K     <- length(s0)
   d     <- diag(crossprod(X))
   alpha <- matrix(0,2,K)
   mu    <- matrix(0,2,K)
   s     <- matrix(0,2,K)
   for (i in 1:2) {
-    shat   <- sigma/d[i]
-    s1     <- 1/(1/shat + 1/(sigma*sa))
-    mu1    <- s1/shat*bhat[i]
-    logBF  <- log(sqrt(shat)/sqrt(shat + sigma*sa)) + mu1^2/(2*s1)
+    shat   <- sigma/sqrt(d[i])
+    s1     <- 1/sqrt(1/shat^2 + 1/s0^2)
+    mu1    <- (s1/shat)^2*bhat[i]
+    logBF  <- log(shat/sqrt(shat^2 + s0^2)) + (mu1/s1)^2/2
     logp1  <- log(w) + logBF
     mu[i,] <- mu1
-    s[i,]  <- s1
+    s[i,]  <- s1^2
     alpha[i,] <- normalizelogweights(logp1)
   }
   b <- rowSums(alpha * mu)
   print(b)
-  elbo <- (-n/2*log(2*pi*sigma)
-           - (norm2(y - X %*% b)^2 + sum(d*betavarmix(alpha,mu,s)))/(2*sigma))
+  elbo <- (-n/2*log(2*pi*sigma^2)
+           - (norm2(y - X %*% b)^2 
+              + sum(d*betavarmix(alpha,mu,s)))/(2*sigma^2))
   for (i in 1:K)
     elbo <- (elbo + sum(alpha[,i]*log(w[i] + 1e-15)) 
                   - sum(alpha[,i]*log(alpha[,i] + 1e-15)))
   for (i in 2:K)
-    elbo <- (elbo+(sum(alpha[,i]) + sum(alpha[,i]*log(s[,i]/(sigma*sa[i]))))/2
-             - sum(alpha[,i]*(s[,i] + mu[,i]^2))/(sigma*sa[i])/2)
+    elbo <- (elbo+(sum(alpha[,i]) + sum(alpha[,i]*log(s[,i]/(s0[i]^2))))/2
+             - sum(alpha[,i]*(s[,i] + mu[,i]^2))/(s0[i]^2)/2)
   print(elbo,digits = 8)
   return(elbo)
 }
 theta0 <- rnorm(2)
-out <- optim(theta0,function (par) -compute_elbo(par,X,y,s^2,(s0/s)^2,w0),
+out <- optim(theta0,function (par) -compute_elbo(par,X,y,s,s0,w0),
              method = "Nelder-Mead",control = list(reltol = 1e-10))
 print(best)
 print(max(fit$logZ),digits = 10)
