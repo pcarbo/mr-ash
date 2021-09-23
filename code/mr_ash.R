6a8f587ad05f69c087729f4aef27363c866dbadb
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-09-23 08:37:09 -0500

    Modified bayes_lr_ridge_ss and bayes_lr_mix_ss to take summary statistics as input (specifically, bhat and shat).

--- code/mr_ash.R
+++ code/mr_ash.R
@@ -7,7 +7,8 @@
 mr_ash <- function (X, y, se, s0, w0, b, method = c("cd", "nm", "bfgs"),
                     maxiter = 100, tol = 1e-8, update.se = TRUE,
                     update.w0 = TRUE, verbose = TRUE) {
-
+  method <- match.arg(method)
+    
   # Center X and y.
   X <- scale(X,scale = FALSE)
   y <- y - mean(y)
@@ -110,8 +111,9 @@ mr_ash_update_cd <- function (X, y, b, se, s0, w0) {
   erss       <- 0
   
   # Compute the expected residuals.
-  r <- drop(y - X %*% b)
-
+  r  <- drop(y - X %*% b)
+  xx <- diag(crossprod(X))
+  
   # Repeat for each predictor.
   for (i in 1:p) {
     x <- X[,i]
@@ -119,11 +121,18 @@ mr_ash_update_cd <- function (X, y, b, se, s0, w0) {
     # Disregard the ith predictor in the expected residuals.
     r <- r + x*b[i]
 
+    # Compute the least-squares estimate of the coefficients (bhat)
+    # and its standard error (shat) in the siimple linear regression 
+    # r = x*b + e, e ~ N(0,se).
+    bhat <- dot(x,r)/xx[i]
+    shat <- se/xx[i]
+    
     # Update the posterior distribution of the regression coefficients
     # for the ith predictor.
-    out   <- bayes_lr_mix(x,r,se,s0,w0)
-    b[i]  <- out$mu1
-    w0.em <- w0.em + out$w1
+    out     <- bayes_lr_mix_ss(bhat,shat,se,s0,w0)
+    b[i]    <- out$mu1
+    w0.em   <- w0.em + out$w1
+    loglik0 <- ldnorm(r,se)
 
     # Calculate the ith term in the "sum of variances" for the ELBO.
     v <- norm2(x)^2 * out$s1
@@ -136,7 +145,7 @@ mr_ash_update_cd <- function (X, y, b, se, s0, w0) {
     # the posterior and prior distributions of the regression
     # coefficient for the ith predictor.
     erssi <- norm2(r)^2 + v
-    d     <- elbo_const - (out$logbf + out$loglik0 + erssi/(2*se))
+    d     <- elbo_const - (out$logbf + loglik0 + erssi/(2*se))
     elbo  <- elbo - d
     erss  <- erss + v
   }
@@ -150,10 +159,45 @@ mr_ash_update_cd <- function (X, y, b, se, s0, w0) {
   # update for the mixture weights ("w0.em"), the updated variational
   # lower bound (elbo), and the updated expected residual sum of
   # squares ("erss").
-  return(list(b     = b,
-              w0.em = w0.em/p,
-              elbo  = elbo,
-              erss  = erss))
+  return(list(b = b,w0.em = w0.em/p,elbo = elbo,erss = erss))
+}
+
+# TO DO: Explain here what this function is for, and how to use it.
+mr_ash_update_optim <- function (X, y, b, se, s0, w0,
+                                 method = c("nm", "bfgs")) {
+  method <- match.arg(method)
+    
+  # Recover the parameters we are optimizing.
+  xx   <- diag(crossprod(X))
+  r    <- drop(y - X %*% b)
+  vars <- drop(r %*% X)/xx
+    
+  # Find the parameters (locally) maximizing the variational lower
+  # bound.
+  if (method == "nm")
+    out <- optim(vars,fn,method = method,control = list(reltol = 1e-12))
+  else if (method == "bfgs") {
+    # TO DO.
+  }
+  
+  # Complete the computation of the expected residual sum of squares
+  # (erss) and the variational lower bound (elbo).
+  erss <- erss + norm2(r)^2
+  elbo <- elbo - erss/(2*se)
+  
+  # Output the updated posterior mean coefficients ("b"), the M-step
+  # update for the mixture weights ("w0.em"), the updated variational
+  # lower bound (elbo), and the updated expected residual sum of
+  # squares ("erss").
+  return(list(b = b,w0.em = w0.em/p,elbo = elbo,erss = erss))
+}
+
+# TO DO: Explain here what this function does, and how to use it.
+compute_elbo_ss <- function (X, y, b, se) {
+
+  #  updated variational
+  # lower bound (elbo), and the updated expected residual sum of
+  # squares ("erss").
 }
 
 # Fit a univariate linear regression model in which the regression
@@ -164,34 +208,20 @@ mr_ash_update_cd <- function (X, y, b, se, s0, w0) {
 # tried to make the code as simple as possible, with an emphasis on
 # clarity. Very little effort has been devoted to making the
 # implementation efficient, or the code concise.
-bayes_lr_ridge <- function (x, y, se, s0) {
+bayes_lr_ridge_ss <- function (bhat, shat, se, s0) {
 
-  # Compute the least-squares estimate of the coefficients (bhat) and
-  # its standard error (s).
-  xx   <- norm2(x)^2
-  bhat <- dot(x,y)/xx
-  s    <- se/xx
-  
   # Compute the posterior mean (mu1) and variance (s1) assuming a
   # normal prior wiith zero mean and variance s0.
-  s1  <- s0/(1 + s0/s)
-  mu1 <- s1/s*bhat
+  s1  <- s0/(1 + s0/shat)
+  mu1 <- s1/shat * bhat
 
-  # Compute the "null" log-likelihood.
-  loglik0 <- ldnorm(y,se)
-  
   # Compute the log-Bayes factor.
-  logbf <- ldnorm(bhat,s0 + s) - ldnorm(bhat,s)
+  logbf <- log(shat/(s0 + shat))/2 + mu1^2/(2*s1)
 
-  # Return the least-squares estimate (bhat) and its variance (s), the
-  # posterior mean (mu1) and variance (S1), the log-likelihood for the
-  # "null" model (when b = 0), and the log-Bayes factor (logbf).
-  return(list(bhat    = bhat,
-              s       = s,
-              mu1     = mu1,
-              s1      = s1,
-              loglik0 = loglik0,
-              logbf   = logbf))
+  # Return the posterior mean (mu1) and variance (s1), the
+  # log-likelihood for the "null" model (when b = 0), and the
+  # log-Bayes factor (logbf).
+  return(list(mu1 = mu1,s1 = s1,logbf = logbf))
 }
 
 # Fit a univariate linear regression model in which the regression
@@ -201,7 +231,7 @@ bayes_lr_ridge <- function (x, y, se, s0) {
 # tried to make the code as simple as possible, with an emphasis on
 # clarity. Very little effort has been devoted to making the
 # implementation efficient, or the code concise.
-bayes_lr_mix <- function (x, y, se, s0, w0) {
+bayes_lr_mix_ss <- function (bhat, shat, se, s0, w0) {
 
   # Get the number of mixture components.
   k <- length(w0)
@@ -209,7 +239,7 @@ bayes_lr_mix <- function (x, y, se, s0, w0) {
   # Compute the quantities separately for each mixture component.
   out <- vector("list",k)
   for (i in 1:k)
-    out[[i]] <- bayes_lr_ridge(x,y,se,s0[i])
+    out[[i]] <- bayes_lr_ridge_ss(bhat,shat,se,s0[i])
 
   # Compute the posterior assignment probabilities for the latent
   # indicator variable.
@@ -236,11 +266,7 @@ bayes_lr_mix <- function (x, y, se, s0, w0) {
   s1 <- s1 - mu1^2
   
   # Return the the posterior mean (mu1) and svariance (s1), the
-  # posterior assignment probabilities (w1), the log-likelihood for
-  # the "null" model (when b = 0), and the log-Bayes factor (logbf).
-  return(list(mu1     = mu1,
-              s1      = drop(s1),
-              w1      = w1,
-              loglik0 = out[[1]]$loglik0,
-              logbf   = logbf))
+  # posterior assignment probabilities (w1) and the log-Bayes factor
+  # (logbf).
+  return(list(mu1 = mu1,s1 = drop(s1),w1 = w1,logbf = logbf))
 }
